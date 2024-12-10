d4ff3575ed7bd8f7cfb033dd997d055a3f4297a9
rupalbhure <rupalkbhure21@gmail.com>
2024-12-10 17:11:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let colorChangeEl = document.getElementById("colorChange");
+
+function onColorGrey(){
+
+    colorChangeEl.style.backgroundColor = "grey";
+
+}
+
+function onColorGreen(){
+
+    colorChangeEl.style.backgroundColor = "green";
+
+}
+
+function onColorBlue(){
+
+    colorChangeEl.style.backgroundColor = "blue";
+
+}
+
+function onColorYellow(){
+
+    colorChangeEl.style.backgroundColor = "yellow";
+
+}
